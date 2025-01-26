2b4ffdb05a4abe084af57087771d18308673fe4f
jlinares30 <jorgelinaresrrs306@hotmail.com>
2025-01-26 17:31:06 -0500

    modal added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App flex max-md:flex-col">
+    <div className="App relative flex max-md:flex-col">
       <main className='mr-10'>
         <h1 className='text-black font-extrabold text-4xl mb-10' >Desserts</h1>
         <section className='grid gap-6 lg:grid-cols-2 xl:grid-cols-3'>
@@ -24,6 +24,7 @@ function App() {
       <aside className=''>
         <ListCart />
       </aside>
+      {/* <ModalOrderCard /> */}
     </div>
   )
 }
--- src/components/ModalOrderCard.jsx
+++ src/components/ModalOrderCard.jsx
@@ -2,33 +2,35 @@ import styles from '../styles/ModalOrderCard.module.css';
 
 function ModalOrderCard(){
     return (
-        <div className={styles.modalContainer}>
-            <img className={styles.svgSuccess} src="src/images/icon-order-confirmed.svg" alt="icon confirmed" />
-            <h1 className={styles.confirmTitle}>Order Confirmed</h1>
-            <p className={styles.descriptionConfirm}>We hope you enjoy it!</p>
-            <div className={styles.listContainer}>
-                <ul className={styles.cartList}>
-                    <li className={styles.cartItem}>
-                        <div className={styles.productContainer}>
-                            <img className={styles.imageProduct} src="src/images/image-waffle-thumbnail.jpg" alt="image product" />
-                            <div>
-                                <p className={styles.nameProduct}>Classic Tiramisu</p>
-                                <div className={styles.infoContainer}>
-                                    <div className={styles.quantity}>4x</div>
-                                    <div className={styles.priceUnid}>@ $7.00</div>
+        <div className={styles.modalBG}>
+            <div className={styles.modalContainer}>
+                <img className={styles.svgSuccess} src="src/images/icon-order-confirmed.svg" alt="icon confirmed" />
+                <h1 className={styles.confirmTitle}>Order Confirmed</h1>
+                <p className={styles.descriptionConfirm}>We hope you enjoy it!</p>
+                <div className={styles.listContainer}>
+                    <ul className={styles.cartList}>
+                        <li className={styles.cartItem}>
+                            <div className={styles.productContainer}>
+                                <img className={styles.imageProduct} src="src/images/image-waffle-thumbnail.jpg" alt="image product" />
+                                <div>
+                                    <p className={styles.nameProduct}>Classic Tiramisu</p>
+                                    <div className={styles.infoContainer}>
+                                        <div className={styles.quantity}>4x</div>
+                                        <div className={styles.priceUnid}>@ $7.00</div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className={styles.price}>$28.00</div>
-                        <hr className={styles.divider} />
-                    </li>
-                </ul>
-                <div className={styles.totalContainer}>
-                    <p className={styles.orderTotal}>Order Total</p>
-                    <p className={styles.totalPrice}>$46.50</p>
+                            <div className={styles.price}>$28.00</div>
+                            <hr className={styles.divider} />
+                        </li>
+                    </ul>
+                    <div className={styles.totalContainer}>
+                        <p className={styles.orderTotal}>Order Total</p>
+                        <p className={styles.totalPrice}>$46.50</p>
+                    </div>
                 </div>
+                <button className={styles.confirmBtn}>Start New Order</button>
             </div>
-            <button className={styles.confirmBtn}>Start New Order</button>
         </div>
     )
 }
